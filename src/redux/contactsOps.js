eb1305d1b58615c6a1f34a29c3dacf0bedd63052
KateMassa <catherineorlova27@gmail.com>
2024-12-26 16:59:10 +0100

    Update contactsOps.js

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://66b50c3a9f9169621ea54ade.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.goit.global";
 
 const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
